e0642ea368b85bd6d53fe0a48e75e42754a606f1
Nicholas1126 <282235411@qq.com>
2018-12-28 00:11:03 +0800

    Add libpcap build script to project

--- src/libpcap/build.sh
+++ src/libpcap/build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 source env.src
+rm -rf libpcap-1.8.1
 tar -xvzf libpcap-1.8.1.tar.gz
 
 cd libpcap-1.8.1
-./configure --prefix=$ROOT --host=$TARGET_HOST --target=$TARGET_HOST --with-pcap=linux
-make -j 
+./configure --prefix=/home/nicholas/sources/embedded-toolkit/toolchains/aarch64-none-linux-gnu/host --host=aarch64-none-linux-gnu --with-pcap=linux CC="${CROSS_COMPILE}gcc"
+make
 make install prefix=$ROOT
